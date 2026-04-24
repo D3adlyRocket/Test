b5b613bbbec9902b7dd31bddee135c1abc736c3c
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-04-24 23:47:44 +0100

    Update moviesmod.js

--- providers/moviesmod.js
+++ providers/moviesmod.js
@@ -1,84 +1,90 @@
 "use strict";
 
 var TMDB_API_KEY = "439c478a771f35c05022f9feabcca01c";
-var STREAMFLIX_API_BASE = "https://api.streamflix.app";
+var SF_BASE = "https://api.streamflix.app";
 
-// Pre-define Headers (Avoiding Object.assign for old TV engines)
-var BASE_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; TV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-    'Accept': 'application/json',
-    'Referer': 'https://streamflix.app/'
-};
+// We use a simplified helper because many TV boxes crash on 'Object.assign'
+function getJson(url) {
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+            'Accept': 'application/json'
+        }
+    }).then(function(res) {
+        return res.json();
+    });
+}
 
 function getStreams(tmdbId, mediaType, season, episode) {
     var isTV = (mediaType === 'tv' || mediaType === 'series');
-    var tmdbUrl = "https://api.themoviedb.org/3/" + (isTV ? "tv" : "movie") + "/" + tmdbId + "?api_key=" + TMDB_API_KEY;
+    var typePath = isTV ? "tv" : "movie";
+    var tmdbUrl = "https://api.themoviedb.org/3/" + typePath + "/" + tmdbId + "?api_key=" + TMDB_API_KEY;
 
-    return fetch(tmdbUrl, { headers: BASE_HEADERS })
-        .then(function(res) { return res.json(); })
-        .then(function(tmdbData) {
-            var searchTitle = (isTV ? tmdbData.name : tmdbData.title).toLowerCase();
+    return getJson(tmdbUrl).then(function(tmdb) {
+        var query = (isTV ? tmdb.name : tmdb.title).toLowerCase();
+
+        // 1. Get Config
+        return getJson(SF_BASE + "/config/config-streamflixapp.json").then(function(config) {
             
-            // Fetch Config and Data sequentially to avoid Promise.all (can be buggy on old TVs)
-            return fetch(STREAMFLIX_API_BASE + "/config/config-streamflixapp.json", { headers: BASE_HEADERS })
-                .then(function(r) { return r.json(); })
-                .then(function(config) {
-                    return fetch(STREAMFLIX_API_BASE + "/data.json", { headers: BASE_HEADERS })
-                        .then(function(r) { return r.json(); })
-                        .then(function(data) {
-                            
-                            var list = data.data || [];
-                            var entry = null;
-                            for (var i = 0; i < list.length; i++) {
-                                if (list[i].moviename && list[i].moviename.toLowerCase().indexOf(searchTitle) !== -1) {
-                                    entry = list[i];
-                                    break;
-                                }
-                            }
+            // 2. Get Data
+            return getJson(SF_BASE + "/data.json").then(function(db) {
+                var entry = null;
+                var list = db.data || [];
+                
+                // Simple loop (most compatible)
+                for (var i = 0; i < list.length; i++) {
+                    if (list[i].moviename && list[i].moviename.toLowerCase().indexOf(query) !== -1) {
+                        entry = list[i];
+                        break;
+                    }
+                }
 
-                            if (!entry) return [];
+                if (!entry) return [];
 
-                            var streams = [];
-                            var bases = [];
-                            if (config.premium) bases = bases.concat(config.premium);
-                            if (config.movies) bases = bases.concat(config.movies);
+                var streams = [];
+                var servers = [].concat(config.premium || [], config.movies || []);
 
-                            if (!isTV) {
-                                for (var j = 0; j < bases.length; j++) {
-                                    if (entry.movielink) {
-                                        streams.push({
-                                            name: "StreamFlix",
-                                            title: "Movie | " + entry.moviename,
-                                            url: bases[j] + entry.movielink,
-                                            quality: "1080p"
-                                        });
-                                    }
-                                }
-                                return streams;
-                            } else {
-                                // Direct Firebase REST call
-                                var fbUrl = "https://chilflix-410be-default-rtdb.asia-southeast1.firebasedatabase.app/Data/" + entry.moviekey + "/seasons/" + season + "/episodes.json";
-                                return fetch(fbUrl).then(function(r) { return r.json(); }).then(function(epData) {
-                                    var ep = epData ? (epData[episode - 1] || epData[episode.toString()]) : null;
-                                    if (ep && ep.link) {
-                                        for (var k = 0; k < bases.length; k++) {
-                                            streams.push({
-                                                name: "StreamFlix",
-                                                title: "TV | S" + season + "E" + episode,
-                                                url: bases[k] + ep.link,
-                                                quality: "1080p"
-                                            });
-                                        }
-                                    }
-                                    return streams;
+                if (!isTV) {
+                    // Movie Logic
+                    for (var j = 0; j < servers.length; j++) {
+                        if (entry.movielink) {
+                            streams.push({
+                                name: "StreamFlix",
+                                title: "Movie | " + entry.moviename,
+                                url: servers[j] + entry.movielink,
+                                quality: "1080p"
+                            });
+                        }
+                    }
+                    return streams;
+                } else {
+                    // TV Logic - Using the Firebase .json shortcut which is most TV-friendly
+                    var fbUrl = "https://chilflix-410be-default-rtdb.asia-southeast1.firebasedatabase.app/Data/" + entry.moviekey + "/seasons/" + season + "/episodes.json";
+                    
+                    return getJson(fbUrl).then(function(epData) {
+                        // Support both array and object responses from Firebase
+                        var ep = epData ? (epData[episode - 1] || epData[episode.toString()]) : null;
+                        
+                        if (ep && ep.link) {
+                            for (var k = 0; k < servers.length; k++) {
+                                streams.push({
+                                    name: "StreamFlix",
+                                    title: "TV | S" + season + "E" + episode,
+                                    url: servers[k] + ep.link,
+                                    quality: "1080p"
                                 });
                             }
-                        });
-                });
-        })
-        .catch(function(err) {
-            return [];
+                        }
+                        return streams;
+                    }).catch(function() { return []; });
+                }
+            });
         });
+    }).catch(function(err) {
+        console.error("SF Error: " + err.message);
+        return [];
+    });
 }
 
 if (typeof module !== 'undefined' && module.exports) {

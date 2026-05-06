bf34c33df2c90ed7e8a1fe4ecb376a5bfdb45453
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-05-06 15:58:15 +0100

    Update DOMAIN URL from uhdmovies.rip to uhdmovies.pink

--- providers/fmovies.js
+++ providers/fmovies.js
@@ -1,6 +1,6 @@
 // providers/uhdmovies.js — converted to ES Module
 
-const DOMAIN = "https://uhdmovies.rip";
+const DOMAIN = "https://uhdmovies.pink";
 const TMDB_API = "https://api.themoviedb.org/3";
 const TMDB_API_KEY = "1865f43a0549ca50d341dd9ab8b29f49";
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";

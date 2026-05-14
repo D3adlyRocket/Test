ec4f63e6689d99f74efa52679698bbdbc17a2f14
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-05-14 10:03:20 +0100

    Update region in makeClientInfo_oneroom function

--- providers/test.js
+++ providers/test.js
@@ -285,7 +285,7 @@ function makeClientInfo_mbox(bm) {
 function makeClientInfo_oneroom(bm) {
   var brand = bm ? bm.model : "Pixel 7";
   var model = bm ? bm.brand : "Google";
-  return '{"package_name":"com.community.oneroom","version_name":"3.0.13.0325.03","version_code":50020088,"os":"android","os_version":"13","install_ch":"ps","device_id":"' + DEVICE_ID + '","install_store":"ps","gaid":"1b2212c1-dadf-43c3-a0c8-bd6ce48ae22d","brand":"' + brand + '","model":"' + model + '","system_language":"en","net":"NETWORK_WIFI","region":"US","timezone":"Asia/Calcutta","sp_code":"","X-Play-Mode":"1","X-Idle-Data":"1","X-Family-Mode":"0","X-Content-Mode":"0"}';
+  return '{"package_name":"com.community.oneroom","version_name":"3.0.13.0325.03","version_code":50020088,"os":"android","os_version":"13","install_ch":"ps","device_id":"' + DEVICE_ID + '","install_store":"ps","gaid":"1b2212c1-dadf-43c3-a0c8-bd6ce48ae22d","brand":"' + brand + '","model":"' + model + '","system_language":"en","net":"NETWORK_WIFI","region":"IN","timezone":"Asia/Calcutta","sp_code":"","X-Play-Mode":"1","X-Idle-Data":"1","X-Family-Mode":"0","X-Content-Mode":"0"}';
 }
 
 function buildPostHeaders(url, jsonBody) {

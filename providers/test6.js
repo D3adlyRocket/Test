6e0072f471bf03f4fc3ff01caf0b6740ae059580
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-06-18 08:53:52 +0100

    Add filename to cleanTitle construction

--- providers/test6.js
+++ providers/test6.js
@@ -167,7 +167,7 @@ function makeStream(name, title, url, quality, headers, mediaInfo) {
 
     // 4. CONSTRUCT THE MULTI-LINE DROPDOWN LAYOUT
     if (filename) {
-        cleanTitle = titleRow + '\n📦 💎 ' + displayQuality + ' | English 🇺🇸 • Hindi 🇮🇳' + audioChannelTag + ' |\n🎞️ ' + fileFormat + ' | 💾 ' + fileSize + ' | ☁️ ' + sourceTag + hdrTag + ' |
+        cleanTitle = titleRow + '\n📦 💎 ' + displayQuality + ' | English 🇺🇸 • Hindi 🇮🇳' + audioChannelTag + ' |\n🎞️ ' + fileFormat + ' | 💾 ' + fileSize + ' | ☁️ ' + sourceTag + hdrTag + ' |\n🔗' + filename;
     } else {
         cleanTitle = titleRow + '\n📦 💎 ' + displayQuality + ' | English 🇺🇸 • Hindi 🇮🇳' + audioChannelTag + ' |\n🎞️ ' + fileFormat + ' | 💾 ' + fileSize + ' | ☁️ ' + sourceTag + hdrTag;
     }

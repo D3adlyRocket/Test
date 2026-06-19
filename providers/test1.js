bb2ab46119a4b057682843f4f4e3b508651fba57
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-06-20 00:24:35 +0100

    Update test1.js

--- providers/test1.js
+++ providers/test1.js
@@ -183,9 +183,9 @@ var require_formatter = __commonJS({
         size: finalTitle, 
         providerName: "VixSrc",
         // Trick engine with non-breaking zero width character formulas to swallow "Unknown" and eliminate trailing bullets cross-platform
-        qualityTag: "null",
-        quality: "null",
-        language: "null",
+        qualityTag: "",
+        quality: "",
+        language: "",
         description: finalTitle,
         originalTitle: stream.title || "Stream",
         _nuvio_formatted: true,

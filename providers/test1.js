417254ac11c49900624e4098200912dcf79732f8
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-06-20 11:42:18 +0100

    Fix formatting of quality property in test1.js

--- providers/test1.js
+++ providers/test1.js
@@ -661,7 +661,7 @@ function getStreams(id, type, season, episode, providerContext = null) {
         name: `VixSrc`,
         url: streamUrl,
         easyProxySourceUrl: embedUrl,
-        quality: " ",
+        quality: "",
         type: "direct",
         headers: streamHeaders,
         behaviorHints: {

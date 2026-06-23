7ebe4556d3c6f6e0dd8522169d9eb093f706ffe4
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-06-23 09:26:21 +0100

    Add size property to returned object in test4.js

--- providers/test4.js
+++ providers/test4.js
@@ -195,6 +195,7 @@ function makeStream(rawFilename, url, referer, parsedSize) {
     return {
         name: finalName,
         title: finalTitle,
+        size: finalTitle
         url: url.replace(/ /g, "%20"),
         behaviorHints: {
             notWebReady: true,
